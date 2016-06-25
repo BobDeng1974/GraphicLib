5a0b929ea74969cfb4c6e4d52b4965e0946e8c7f
SATAN_Z <249002031@qq.com>
2016-06-25 16:48:33 +0800

    存档

---
+++ source/01_project_skeleton/GLClass/Window.cpp
@@ -0,0 +1,16 @@
+//
+//  Window.cpp
+//  opengl-series
+//
+//  Created by SATAN_Z on 16/6/24.
+//
+//
+
+#include "Window.hpp"
+
+
+using namespace zdogl;
+
+Window::Window(){
+    
+}
---
+++ source/01_project_skeleton/GLClass/Window.hpp
@@ -0,0 +1,31 @@
+//
+//  Window.hpp
+//  opengl-series
+//
+//  Created by SATAN_Z on 16/6/24.
+//
+//
+
+#ifndef Window_hpp
+#define Window_hpp
+
+#include <stdio.h>
+
+#include <GLFW/glfw3.h>
+#include "Object.hpp"
+
+namespace zdogl {
+    
+    class Window : public Object {
+        
+        Window();
+        ~Window();
+        
+        
+        
+    };
+    
+}
+
+
+#endif /* Window_hpp */
