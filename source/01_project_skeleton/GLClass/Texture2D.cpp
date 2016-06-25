2000ac01b4f0e75c6ce9d9f089785dc6f00126ee
SATAN_Z <249002031@qq.com>
2016-06-26 02:46:27 +0800

    纹理已完成

--- source/01_project_skeleton/GLClass/Texture2D.cpp
+++ source/01_project_skeleton/GLClass/Texture2D.cpp
@@ -7,3 +7,65 @@
 //
 
 #include "Texture2D.hpp"
+
+
+using namespace zdogl;
+
+static GLenum TextureFormatForBitmapFormat(ze::Bitmap::Format format)
+{
+    switch (format) {
+        case ze::Bitmap::Format_Grayscale: return GL_LUMINANCE;
+        case ze::Bitmap::Format_GrayscaleAlpha: return GL_LUMINANCE_ALPHA;
+        case ze::Bitmap::Format_RGB: return GL_RGB;
+        case ze::Bitmap::Format_RGBA: return GL_RGBA;
+        default: throw std::runtime_error("Unrecognised Bitmap::Format");
+    }
+}
+
+Texture2D::Texture2D(){
+    glGenTextures(1 , &_handle);
+}
+
+Texture2D::~Texture2D(){
+    glDeleteTextures(1 , &_handle);
+}
+
+bool Texture2D::init(const ze::Bitmap &bitmap ,
+                     GLint minMagFiler /* = GL_LINEAR */ ,
+                     GLint wrapMode /* = GL_CLAMP_TO_EDGE */){
+    
+    bind();
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, minMagFiler);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, minMagFiler);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
+    glTexImage2D(GL_TEXTURE_2D,
+                 0,
+                 TextureFormatForBitmapFormat(bitmap.getFormat()),
+                 (GLsizei)bitmap.getWidth(),
+                 (GLsizei)bitmap.getHeight(),
+                 0,
+                 TextureFormatForBitmapFormat(bitmap.getFormat()),
+                 GL_UNSIGNED_BYTE,
+                 bitmap.getPixelBuffer());
+    unbind();
+    
+    return  true;
+}
+
+Texture2D::Texture2D(const ze::Bitmap &bitmap ,
+                     GLint minMagFiler /* = GL_LINEAR */ ,
+                     GLint wrapMode /* = GL_CLAMP_TO_EDGE */){
+    glGenTextures(1 , &_handle);
+    init(bitmap , minMagFiler , wrapMode);
+}
+
+Texture2D * Texture2D::loadFromFile(std::string & filePath ,
+                                    GLint minMagFiler /* = GL_LINEAR */ ,
+                                    GLint wrapMode /* = GL_CLAMP_TO_EDGE */){
+    Texture2D * ret = new Texture2D();
+    ze::Bitmap bitmap = ze::Bitmap::loadFromFile(filePath);
+    ret->init(bitmap , minMagFiler , wrapMode);
+    return ret;
+}
--- source/01_project_skeleton/GLClass/Texture2D.hpp
+++ source/01_project_skeleton/GLClass/Texture2D.hpp
@@ -11,6 +11,7 @@
 
 #include "Object.hpp"
 #include "Bitmap.hpp"
+#include <string>
 
 namespace zdogl {
     
@@ -33,6 +34,32 @@ namespace zdogl {
                 GLint minMagFiler = GL_LINEAR,
                 GLint wrapMode = GL_CLAMP_TO_EDGE);
 
+        static Texture2D * loadFromFile(std::string & filePath ,
+                                        GLint minMagFiler = GL_LINEAR,
+                                        GLint wrapMode = GL_CLAMP_TO_EDGE);
+        
+        /**
+         init a texture from a bitmap.
+         
+         The texture will be loaded upside down because tdogl::Bitmap pixel data
+         is ordered from the top row down, but OpenGL expects the data to
+         be from the bottom row up.
+         
+         @param bitmap  The bitmap to load the texture from
+         @param minMagFiler  GL_NEAREST or GL_LINEAR
+         @param wrapMode GL_REPEAT, GL_MIRRORED_REPEAT, GL_CLAMP_TO_EDGE, or GL_CLAMP_TO_BORDER
+         */
+        bool init(const ze::Bitmap& bitmap,
+                  GLint minMagFiler = GL_LINEAR,
+                  GLint wrapMode = GL_CLAMP_TO_EDGE);
+        
+        inline void bind(){
+            glBindTexture(GL_TEXTURE_2D , _handle);
+        }
+        
+        inline void unbind(){
+            glBindTexture(GL_TEXTURE_2D , 0);
+        }
         
         Texture2D();
         
@@ -41,9 +68,20 @@ namespace zdogl {
          */
         ~Texture2D();
         
-        GLfloat originalWidth() const;
+        inline GLfloat originalWidth() const{
+            return _originalWidth;
+        }
         
-        GLfloat originalHeight() const;
+        GLfloat originalHeight() const{
+            return _originalHeight;
+        }
+        
+    private:
+        
+        GLfloat _originalWidth;
+        
+        GLfloat _originalHeight;
+        
         
     };
     
@@ -51,3 +89,8 @@ namespace zdogl {
 
 
 #endif /* Texture2D_hpp */
+
+
+
+
+
