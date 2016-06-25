4ddb78c6f63679d9eb988904dfa5f5596bb36b8b
SATAN_Z <249002031@qq.com>
2016-06-25 18:33:14 +0800

    shader

--- source/01_project_skeleton/GLClass/Shader.cpp
+++ source/01_project_skeleton/GLClass/Shader.cpp
@@ -7,3 +7,74 @@
 //
 
 #include "Shader.hpp"
+#include <fstream>
+#include <sstream>
+
+using namespace zdogl;
+
+Shader::Shader(const std::string& shaderCode, GLenum shaderType) {
+    
+    //create the shader object
+    _handle = glCreateShader(shaderType);
+    if(_handle == 0)
+        throw std::runtime_error("glCreateShader failed");
+    
+    //set the source code
+    const char* code = shaderCode.c_str();
+    glShaderSource(_handle, 1, (const GLchar**)&code, NULL);
+    
+    //compile
+    glCompileShader(_handle);
+    
+    //throw exception if compile error occurred
+    GLint status;
+    glGetShaderiv(_handle, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        std::string msg("Compile failure in shader:\n");
+        
+        GLint infoLogLength;
+        glGetShaderiv(_handle, GL_INFO_LOG_LENGTH, &infoLogLength);
+        char* strInfoLog = new char[infoLogLength + 1];
+        glGetShaderInfoLog(_handle, infoLogLength, NULL, strInfoLog);
+        msg += strInfoLog;
+        delete[] strInfoLog;
+        
+        glDeleteShader(_handle); _handle = 0;
+        throw std::runtime_error(msg);
+    }
+    
+}
+
+bool Shader::init(){
+    return  true;
+}
+
+Shader::Shader(const Shader& other){
+    
+}
+
+Shader::~Shader() {
+
+}
+
+Shader& Shader::operator = (const Shader& other) {
+    _handle = other._handle;
+    return * this;
+}
+
+Shader Shader::create(std::string &filePath, GLenum shaderType){
+    //open file
+    std::ifstream f;
+    f.open(filePath.c_str(), std::ios::in | std::ios::binary);
+    if(!f.is_open()){
+        throw std::runtime_error(std::string("Failed to open file: ") + filePath);
+    }
+    
+    //read whole file into stringstream buffer
+    std::stringstream buffer;
+    buffer << f.rdbuf();
+    
+    //return new shader
+    Shader shader(buffer.str(), shaderType);
+    return shader;
+}
--- source/01_project_skeleton/GLClass/Shader.hpp
+++ source/01_project_skeleton/GLClass/Shader.hpp
@@ -10,5 +10,54 @@
 #define Shader_hpp
 
 #include <stdio.h>
+#include "Object.hpp"
+
+
+namespace zdogl {
+    
+    /**
+     Represents a compiled OpenGL shader.
+     */
+    class Shader : public Object {
+        
+    public:
+        
+        Shader();
+        
+        ~Shader();
+        
+        virtual bool init();
+        
+        /**
+         Creates a shader from a text file.
+         
+         @param filePath    The path to the text file containing the shader source.
+         @param shaderType  Same as the argument to glCreateShader. For example GL_VERTEX_SHADER
+         or GL_FRAGMENT_SHADER.
+         
+         @throws std::exception if an error occurs.
+         */
+        static Shader create(std::string &filePath , GLenum shaderType);
+        
+        /**
+         Creates a shader from a string of shader source code.
+         
+         @param shaderCode  The source code for the shader.
+         @param shaderType  Same as the argument to glCreateShader. For example GL_VERTEX_SHADER
+         or GL_FRAGMENT_SHADER.
+         
+         @throws std::exception if an error occurs.
+         */
+        Shader(const std::string& shaderCode, GLenum shaderType);
+        
+        Shader(const Shader& other);
+        Shader& operator =(const Shader& other);
+
+        
+    };
+    
+}
+
+
 
 #endif /* Shader_hpp */
