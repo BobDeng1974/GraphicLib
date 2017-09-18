5dd03c734e6a8d223daabd15f4aae354286b9057
SATAN_Z <249002031@qq.com>
2017-09-18 22:53:47 +0800

    删除多余代码

--- source/01_project_skeleton/GLClass/Buffer.cpp
+++ source/01_project_skeleton/GLClass/Buffer.cpp
@@ -43,18 +43,7 @@ _usage(usage){
 
 void Buffer::inflateBuffer(GLuint size , GLvoid * p ,  GLenum usage /*= GL_STATIC_DRAW*/){
     bind();
-    switch (_bufferType) {
-        case GL_ARRAY_BUFFER:
-            glBufferData(_bufferType , size , p , usage);
-            break;
-            
-        case GL_ELEMENT_ARRAY_BUFFER:
-            glBufferData(_bufferType , size , p , usage);
-            break;
-            
-        default:
-            break;
-    }
+    glBufferData(_bufferType , size , p , usage);
     _usage = usage;
     _dataSize = size;
 }
